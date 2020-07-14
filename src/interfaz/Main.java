6cffeb606fa375c4f69705bb0488126212531935
dm94 <dm94@hotmail.es>
2020-07-14 14:10:06 +0200

    change name

--- src/interfaz/Main.java
+++ src/interfaz/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("WooPOS");
+        this.primaryStage.setTitle("WooTPV");
 
         Parent root = FXMLLoader.load(getClass().getResource("fxml/Principal.fxml"));
 
